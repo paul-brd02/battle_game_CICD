c16f127214f5b6ff9e54c95ef8f7c51d62da4997
paul-brd02 <paul.bardou@live.fr>
2024-06-29 17:45:51 +0200

    Test est mort

--- tests/test.py
+++ tests/test.py
@@ -6,3 +6,7 @@ class TestPersonnage(TestCase):
     def test_point_de_vie(self):
         personnage = Personnage()
         self.assertEqual(100, personnage.ptv)
+
+    def test_tuer(self):
+        personnage = Personnage()
+        self.assertFalse(personnage.est_mort)
