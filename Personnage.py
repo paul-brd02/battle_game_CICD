901ce05716ffcd16f12674b188d8038aae46cd80
paul-brd02 <paul.bardou@live.fr>
2024-06-29 18:19:07 +0200

    Ajout getter et setter & test attaque & point d'attaque

--- Personnage.py
+++ Personnage.py
@@ -1,15 +1,40 @@
 class Personnage():
     def __init__(self) -> None:
-        self.ptv = 100
-        self.est_mort = False
+        self.__point_de_vie = 100
+        self.__point_attaque = 50
+        self.__est_mort = False
+
+    def get_point_de_vie(self):
+        return self.__point_de_vie
+
+    def get_point_attaque(self):
+        return self.__point_attaque
+
+    def get_est_mort(self):
+        return self.__est_mort
+
+    def set_point_de_vie(self, value: int):
+        self.__point_de_vie = value
+
+    def set_point_attaque(self, value: int):
+        self.__point_attaque = value
+
+    def set_est_mort(self):
+        self.__est_mort = True
 
     def tuer(self):
-        self.est_mort = True
-        self.ptv = 0
+        self.set_point_de_vie(0)
+        self.set_est_mort()
+    
+    def reduire_point_de_vie(self, value: int):
+        self.set_point_de_vie(self.__point_de_vie - value)
     
     def attaque(self, personnage:'Personnage'):
-        degat = 200
-        if degat > personnage.ptv:
-            personnage.ptv = 0
+        attaque = self.get_point_attaque()
+        if attaque > personnage.get_point_de_vie():
+            personnage.set_point_de_vie(0)
         else:
-            personnage.ptv -= degat
+            personnage.reduire_point_de_vie(attaque)
+    
+
+    
